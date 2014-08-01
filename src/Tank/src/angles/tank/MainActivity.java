9e53421d204c72ef0b4adecba60c2cf62433c5ce
Sampson <dujianwxfdt@live.cn>
2014-08-01 21:20:48 +0800

    Create Project

---
+++ src/Tank/src/angles/tank/MainActivity.java
@@ -0,0 +1,20 @@
+package angles.tank;
+
+import android.app.Activity;
+import android.os.Bundle;
+//import android.view.Menu;
+//import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weclome_layout);
+        
+        
+        
+        
+        
+    }
+}
